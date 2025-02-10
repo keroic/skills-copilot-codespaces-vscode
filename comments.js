a9195609ce66753a1495317d690bf038e809ca45
keroic <keroic@gmail.com>
2025-02-10 02:27:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,47 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the fs module to read the file and send it to the client.   
+// The comments.html file should be in the same directory as the comments.js file.
+// The comments.html file should contain the following HTML code:
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>Comments</title>
+// </head>
+// <body>
+//   <h1>Comments</h1>
+//   <ul>
+//     <li>Comment 1</li>
+//     <li>Comment 2</li>
+//     <li>Comment 3</li>
+//   </ul>
+// </body>
+// </html>
+// The server should respond to the following URL:
+// http://localhost:3000/comments
+// The server should respond with the comments.html file.
+// The server should respond to all other URLs with a 404 status code and the following message:
+// Page not found
+// The server should respond with the following headers:
+// Content-Type: text/html
+// Content-Length: {length of the response}
+// The server should respond with the following status codes:
+// 200 for a successful response
+// 404 for a not found response
+// The server should respond with the following body:
+// The contents of the comments.html file for a successful response
+// Page not found for a not found response
+// The server should use the fs module to read the comments.html file.
+// The server should use the http module to create the web server.
+// The server should use the createServer method to create the web server.
+// The server should use the listen method to listen on port 3000.
+// The server should use the request and response objects to respond to requests.
+// The server should use the fs.readFile method to read the comments.html file.
+// The server should use the response.writeHead method to set the headers.
+// The server should use the response.end method to send the response.
+// The server should use the response.statusCode property to set the status code.
+// The server should use the response.end method to send the response body.
+// The server should use the response.end method to send the response body.
+// The server should use the response.end method to send the response body.
+// The server should use the response.end method to send the response body.
+// The server should use the response.end method to send the response body.
+// The server should use the response.end method to send the response body.
